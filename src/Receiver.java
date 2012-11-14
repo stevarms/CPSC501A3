1239683de369d881cd3413b1fa3a6cf239c9dc4e
Stephen Armstrong <stevarms@gmail.com>
2012-11-13 21:44:29 -0700

    completed serializer and deserializer, added functionality to receiver

--- src/Receiver.java
+++ src/Receiver.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.net.*;
-
+import org.jdom2.input.SAXBuilder;
 import org.jdom2.Document;
+import org.jdom2.JDOMException;
 
 public class Receiver {
 	public static void main(String[] args) throws IOException {
@@ -14,30 +15,43 @@ public class Receiver {
 			System.err.println("Could not listen on port: " + port);
 			System.exit(1);
 		}
-		System.out.println("Waiting for client request");
 		while (true) {
+			System.out.println("Waiting for client request");
+			File aFile = new File("recdata.xml");
 			// listen for client
-			Socket clientSocket = null;
+			Socket s = null;
 			try {
-				clientSocket = serverSocket.accept();
+				s = serverSocket.accept();
 				System.out.println("Client connected");
 			} catch (IOException e) {
 				System.err.println("Accept failed.");
 				System.exit(1);
 			}
-			
-			//create the socket data streams
-            DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
-            BufferedReader inFromClient = new BufferedReader(
-                    new InputStreamReader(
-                    clientSocket.getInputStream()));
-            //initialize the strings and protocol
-            String inputLine;
 
-            //listen for the client response 
-            while ((inputLine = inFromClient.readLine()) != null) {
-            	System.out.println(inputLine);
-            }
+			InputStream input = s.getInputStream();
+			FileOutputStream out = new FileOutputStream(aFile);
+
+			byte[] buffer = new byte[1024 * 1024];
+
+			int bytesReceived = 0;
+			System.out.println("receiving file");
+			while ((bytesReceived = input.read(buffer)) > 0) {
+				out.write(buffer, 0, bytesReceived);
+				System.out.println(bytesReceived + " Bytes received");
+				break;
+			}
+			SAXBuilder builder = new SAXBuilder();
+			Object obj = null;
+			try {
+				Document doc = (Document) builder.build(aFile);
+				obj = Deserializer.deserialize(doc);
+			} catch (JDOMException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Inspector inspector = new Inspector();
+			inspector.inspect(obj, true);
 		}
 	}
 }
--- src/Sender.java
+++ src/Sender.java
@@ -9,7 +9,7 @@ public class Sender {
 
 	public static void main(String[] args) throws Exception {
 		String server = "localhost";
-		int port  = Integer.parseInt("3333");
+		int port = Integer.parseInt("3333");
 		objList = new ArrayList<Object>();
 		boolean exit_flag = false;
 		while (!exit_flag) {
@@ -25,7 +25,7 @@ public class Sender {
 					System.out.println("what kind of object?");
 					System.out.println("1. Simple Object(int,boolean)");
 					System.out.println("2. Simple Object With Reference");
-					System.out.println("3. Array");
+					System.out.println("3. Array(int[])");
 					System.out.println("4. Array with References");
 					System.out.println("5. Java Collection Class");
 					System.out.println("6. Back");
@@ -35,13 +35,14 @@ public class Sender {
 					input = bufferRead.readLine();
 
 					if (input.equals("1")) {
-						System.out.println("enter field values.  Eg: '1,true'");
+						System.out.println("Enter field values.  Eg: '1,true'");
 						bufferRead = new BufferedReader(new InputStreamReader(
 								System.in));
 						input = bufferRead.readLine();
 						String[] fields = input.split(",");
 						if (fields.length != 2) {
-							System.out.println("You must enter exactly 2 parameters");
+							System.out
+									.println("You must enter exactly 2 parameters");
 						} else {
 							boolean bool = false;
 							if (fields[1].toLowerCase().equals("true")) {
@@ -49,7 +50,8 @@ public class Sender {
 							} else if (fields[1].toLowerCase().equals("false")) {
 								bool = false;
 							} else {
-								System.out.println("you must enter true or false for a boolean");
+								System.out
+										.println("You must enter true or false for a boolean");
 							}
 							try {
 								int i = Integer.parseInt(fields[0]);
@@ -61,6 +63,23 @@ public class Sender {
 						}
 					} else if (input.equals("2")) {
 					} else if (input.equals("3")) {
+						System.out.println("Enter field values.  Eg: '1,2,3'");
+						bufferRead = new BufferedReader(new InputStreamReader(
+								System.in));
+						input = bufferRead.readLine();
+						String[] fields = input.split(",");
+						int[] ints = new int[fields.length];
+						for (int i = 0; i < ints.length; i++) {
+							try {
+								ints[i] = Integer.parseInt(fields[i]);
+							} catch (Exception e) {
+								System.out
+										.println("You can only enter numbers");
+							}
+						}
+						Object obj = new simpleArray(ints);
+						objList.add(obj);
+
 					} else if (input.equals("4")) {
 					} else if (input.equals("5")) {
 					} else if (input.equals("6")) {
@@ -70,21 +89,39 @@ public class Sender {
 					}
 				} else if (input.equals("2")) {
 					for (Object obj : objList) {
-						//System.out.println(obj.toString());
+						System.out.println("Deserializing object...");
 						XMLOutputter out = new XMLOutputter();
 						Document doc = Serializer.serialize(obj);
-						out.output(doc, System.out);
-					    Socket s = new Socket(server, port);
-					    OutputStream os = s.getOutputStream();
-					    DataOutputStream dos = new DataOutputStream(os);
-					    out.output(doc, dos);
-					    s.close();
+
+						File aFile = new File("sentdata.xml");
+						BufferedWriter writer = new BufferedWriter(
+								new FileWriter(aFile));
+						out.output(doc, writer);
+						writer.close();
+
+						System.out.println("Transferring file...");
+						try {
+							Socket s = new Socket(server, port);
+							OutputStream output = s.getOutputStream();
+							FileInputStream fileInputStream = new FileInputStream(
+									aFile);
+							byte[] buffer = new byte[1024 * 1024];
+							int bytesRead = 0;
+							while ((bytesRead = fileInputStream.read(buffer)) > 0) {
+								output.write(buffer, 0, bytesRead);
+							}
+							fileInputStream.close();
+							s.close();
+						} catch (IOException e) {
+							System.out.println("connection refused");
+						}
+						System.out.println("Transfer Complete");
 					}
 				} else if (input.equals("3")) {
 					System.out.println("Exiting...");
 					exit_flag = true;
 				} else {
-					System.out.println("invalid input");
+					System.out.println("Invalid input");
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
